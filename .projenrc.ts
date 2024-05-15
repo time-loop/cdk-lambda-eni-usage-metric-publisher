fb5b739709ce205b82f692874c9f212eb39ae0b0
ClickUp CI/CD <93961939+cu-infra-svc-git@users.noreply.github.com>
2024-05-15 13:19:56 -0600

    fix(deps): upgrade projen (#14)
    Upgrades project dependencies. See details in [workflow run].
    
    [Workflow Run]: https://github.com/time-loop/cdk-lambda-eni-usage-metric-publisher/actions/runs/9101128192
    
    ------
    
    *Automatically created by projen via the "update-projen-main" workflow*
    
    Signed-off-by: github-actions <github-actions@github.com>
    Co-authored-by: github-actions <github-actions@github.com>

--- .projenrc.ts
+++ .projenrc.ts
@@ -8,19 +8,20 @@ const project = new clickupCdk.ClickUpCdkConstructLibrary({
   authorAddress: 'jamoroso@clickup.com',
   cdkVersion: '2.105.0',
   defaultReleaseBranch: 'main',
-  devDeps: ['@time-loop/clickup-projen', '@aws-cdk/integ-tests-alpha', 'aws-sdk-mock', '@aws-sdk/client-cloudwatch'],
+  experimentalIntegRunner: true,
+  gitignore: ['.vscode/**'],
   projenrcTs: true,
   packageManager: javascript.NodePackageManager.PNPM,
   pnpmVersion: '9',
   repositoryUrl: `https://github.com/time-loop/${name}.git`,
-  gitignore: ['.vscode/**'],
+
   bundledDeps: ['aws-sdk'],
+  devDeps: ['@time-loop/clickup-projen', '@aws-cdk/integ-tests-alpha', 'aws-sdk-mock', '@aws-sdk/client-cloudwatch'],
   peerDeps: ['multi-convention-namer'],
-  experimentalIntegRunner: true,
-  // deps: [] /* Runtime dependencies of this module. */,
-  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-  // packageName: undefined,  /* The "name" in package.json. */
 });
+
+project.npmrc.addConfig('node-linker', 'hoisted'); // PNPM support for bundledDeps https://pnpm.io/npmrc#node-linker
+
 // Assume the usInfraDev role
 const build = project.tryFindObjectFile('.github/workflows/build.yml');
 build?.addOverride('jobs.build.permissions', { 'id-token': 'write' });
